1191e33e1ae9b0ab26ce21c3a144d1b39dd0fef8
andregtech <andreergtech@gmail.com>
2026-03-13 12:45:52 -0300

    Primeiro commit do projeto Drip Store

---
+++ src/pages/layout.jsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div className="min-h-screen flex flex-col overflow-x-hidden">
+      <Header />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
